6f2a9c923db95814081e930aa10d0b7129d901ea
Paul Lime <paullime@gmail.com>
2018-12-20 15:16:31 -0600

    updated event routes

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -9,37 +9,36 @@ const { authenticate } = require('../middleware/authenticate');
 
 module.exports = app => {
 
-  app.post('/api/events',
-    // authenticate, 
-    (req, res) => {
-      console.log(req.body);
+  app.post('/api/events', authenticate, (req, res) => {
 
-      const event = new Event({
-        title: req.body.title,
-        description: req.body.description,
-      });
-
-      event.save().then(event => {
-        res.send(event);
-      })
-        .catch(err => { res.status(500).send(err) });
+    const event = new Event({
+      title: req.body.title,
+      description: req.body.description
     });
 
-  //grab event and push new user to it. check that user is not already in the array.
+    event.save().then(event => {
+      res.send(event);
+    })
+      .catch(err => { res.status(500).send(err) });
+  });
+
+  // JOIN // find event and push/joins new user to it.
   app.post('/api/events/:event_id/join/:user_id', authenticate, (req, res) => {
-    console.log(req.body, "res.body")
+
     Event.findById(req.params.event_id)
       .then((event) => {
-        console.log(event, "event")
-        //if (event.users.length > 100) { res.status(400).send }
+
+        if (event.users.length > 100) res.status(400).send;
         if (!event.users) {
           event.users = [];
-        }
+        };
         if (event.users.indexOf(req.params.user_id) === -1) {
           event.users.push(req.params.user_id);
           event.save();
-        }
+        };
+
         res.status(200).send(event);
+
       })
       .catch(err => {
         console.log(err, "err")
@@ -47,73 +46,62 @@ module.exports = app => {
       })
   });
 
-  app.put('/api/events/:id',
-    // authenticate, 
-    (req, res) => {
-      console.log(req.body);
+  app.put('/api/events/:id', authenticate, (req, res) => {
 
-      Event.findOneAndUpdate(
-        {
-          _id: req.params.id,
-          // userID: req.user._id 
-        },
-        {
-          title: req.body.title,
-          description: req.body.description
-        }
-      ).then(event => {
-        res.send(event);
-      })
-        .catch(err => { res.status(500).send(err) });
-    });
+    Event.findOneAndUpdate(
+      {
+        _id: req.params.id,
+      },
+      {
+        title: req.body.title,
+        description: req.body.description
+      }
+    ).then(event => {
+      res.send(event);
+    })
+      .catch(err => { res.status(500).send(err) });
+  });
 
-  app.get('/api/events',
-    authenticate,
-    (req, res) => {
-      Event.find(
-        // { userID: req.user._id }
-      ).then((events) => {
+  app.get('/api/events', authenticate, (req, res) => {
+    Event.find()
+      .then((events) => {
         res.send({ events })
       }) //{} syntax vs res.json(...map etc.)
-        .catch(err => { res.status(500).send(err) });
-    });
+      .catch(err => { res.status(500).send(err) });
+  });
 
-  app.get('/api/events/:id',
-    // authenticate, 
-    (req, res) => {
+  app.get('/api/events/:id', authenticate, (req, res) => {
 
-      if (!ObjectID.isValid(req.params.id)) {
+    if (!ObjectID.isValid(req.params.id)) {
+      return res.sendStatus(404);
+    }
+
+    Event.findById(req.params.id).then(event => {
+      if (!event) {
         return res.sendStatus(404);
       }
+      res.send({ event });
+    })
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({ error: 'something went wrong' });
+      });
+  });
 
-      Event.findById(req.params.id).then(event => {
-        if (!event) {
-          return res.sendStatus(404);
-        }
-        res.send({ event });
+  app.delete('/api/events/:id', authenticate, (req, res) => {
+
+    if (!ObjectID.isValid(req.params.id)) {
+      return res.status(404).send('Invalid ID');
+    }
+
+    Event.findByIdAndRemove(req.params.id)
+      .then(() => {
+        res.sendStatus(204);
       })
-        .catch(err => {
-          console.error(err);
-          res.status(500).json({ error: 'something went wrong' });
-        });
-    });
-
-  app.delete('/api/events/:id',
-    // authenticate, 
-    (req, res) => {
-
-      if (!ObjectID.isValid(req.params.id)) {
-        return res.status(404).send('Invalid ID');
-      }
-
-      Event.findByIdAndRemove(req.params.id)
-        .then(() => {
-          res.sendStatus(204);
-        })
-        .catch(err => {
-          console.error(err);
-          res.status(500).json({ error: 'something went wrong' });
-        });
-    });
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({ error: 'something went wrong' });
+      });
+  });
 
 };
