24b56e0ad1bf61101185518d9273073fe1873e31
Paul Lime <paullime@gmail.com>
2019-01-08 21:28:03 -0600

    removed unnecessary files

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,3 @@
-exports.API_ORIGIN = process.env.REACT_APP_API_ORIGIN || 'https://plan-it-node.herokuapp.com/';
+exports.API_ORIGIN = process.env.REACT_APP_API_ORIGIN || 'http://localhost:3002';
+
+// 'https://plan-it-node.herokuapp.com/';
--- public/constants.js
+++
@@ -1,2 +0,0 @@
-const TOKEN = 'authToken'
-const ID = 'id'
