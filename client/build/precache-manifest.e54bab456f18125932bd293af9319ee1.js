61be4e1bd5f82c2dadfdf08fc505bfda76e0903e
Paul Lime <paullime@gmail.com>
2019-01-13 12:38:13 -0500

    Adding `build1` to source control

---
+++ client/build/precache-manifest.e54bab456f18125932bd293af9319ee1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e22c34412150ad224c66c2ed9fed4d17",
+    "url": "/static/media/landing-image.e22c3441.jpg"
+  },
+  {
+    "revision": "55273608cedbf992289fde4017215403",
+    "url": "/static/media/blurred-map.55273608.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0e72e543a3d7912244ae",
+    "url": "/static/js/main.0e72e543.chunk.js"
+  },
+  {
+    "revision": "ffc892ceba41973db243",
+    "url": "/static/js/1.ffc892ce.chunk.js"
+  },
+  {
+    "revision": "0e72e543a3d7912244ae",
+    "url": "/static/css/main.ea9d1911.chunk.css"
+  },
+  {
+    "revision": "ea8a6eef50744b669839f5facb91a51b",
+    "url": "/index.html"
+  }
+];
