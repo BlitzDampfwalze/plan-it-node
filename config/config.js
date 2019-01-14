028a73ebe606292572c6ecce9d785da5d6451df8
Paul Lime <paullime@gmail.com>
2019-01-14 07:46:45 -0500

    adding build to src ctrl

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 exports.PORT = process.env.PORT || 3002;
-exports.API_ORIGIN = process.env.REACT_APP_API_ORIGIN || 'https://plan-it-node.herokuapp.com/';
+exports.API_ORIGIN = process.env.REACT_APP_API_ORIGIN || 'https://plan-it-react-capstone.herokuapp.com/';
 exports.DATABASE_URL = process.env.DATABASE_URL;
 exports.TEST_DATABASE_URL = 'mongodb://localhost/plan-it-test'
 exports.JWT_SECRET = process.env.JWT_SECRET;
