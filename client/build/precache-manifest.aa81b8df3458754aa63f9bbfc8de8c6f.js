b144fdeb064092974b0a0b635cde72ae8fbcd82f
Paul Lime <paullime@gmail.com>
2019-02-21 12:38:26 -0600

    updating build

---
+++ client/build/precache-manifest.aa81b8df3458754aa63f9bbfc8de8c6f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e22c34412150ad224c66c2ed9fed4d17",
+    "url": "/static/media/landing-image.e22c3441.jpg"
+  },
+  {
+    "revision": "e2efc19633550b22b16493398b2cb871",
+    "url": "/static/media/iconfinder_cancel_326554.e2efc196.svg"
+  },
+  {
+    "revision": "55273608cedbf992289fde4017215403",
+    "url": "/static/media/blurred-map.55273608.jpg"
+  },
+  {
+    "revision": "45b30f6af163a69443f70b9e363a9a6e",
+    "url": "/static/media/baseline-edit-24px.45b30f6a.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e6a36e02da93e6b3b568",
+    "url": "/static/js/main.e6a36e02.chunk.js"
+  },
+  {
+    "revision": "1a345da4ed5f8fd7d8a0",
+    "url": "/static/js/2.1a345da4.chunk.js"
+  },
+  {
+    "revision": "e6a36e02da93e6b3b568",
+    "url": "/static/css/main.06fcf77e.chunk.css"
+  },
+  {
+    "revision": "c0614e1c2ccc0cc619329a19bda8fc42",
+    "url": "/index.html"
+  }
+];
